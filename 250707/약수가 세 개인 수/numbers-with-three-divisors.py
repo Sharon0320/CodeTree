f8a79b2f5f486c77c937ec1322159bc993b32be1
Lee Jae Yong <sharon0320@gachon.ac.kr>
2025-07-07 14:41:23 +0900

    Title: 약수가 세 개인 수, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250707/약수가 세 개인 수/numbers-with-three-divisors.py
+++ 250707/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -1,12 +1,12 @@
 start, end = map(int, input().split())
 
-# Please write your code here.
 result = []
-for i in range(start,end):
+for i in range(start, end):  # end는 포함 안 되므로 그대로 둠
     temp = []
-    for j in range(1,i+1):
-        if (i%j == 0):
-            temp.append(j)
-    if (len(temp) == 3):
+    for j in range(1, i + 1):  # 1부터 i까지 모든 수로 나누어 보기
+        if i % j == 0:
+            temp.append(j)  # 약수를 temp에 저장
+    if len(temp) == 3:  # 약수가 정확히 3개면 조건 만족
         result.append(i)
-print(len(result))
+
+print(len(result))  # 조건 만족하는 수의 개수 출력
