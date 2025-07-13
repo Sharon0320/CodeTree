f5ffb493ef459f238aceb149e8e64ed9e96e5952
Lee Jae Yong <sharon0320@gachon.ac.kr>
2025-07-14 03:06:09 +0900

    Title: 함수를 이용한 윤년 판별, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250714/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,14 @@
+y = int(input())
+
+# Please write your code here.
+
+def year(n):
+    if (y%100 == 0):
+        if (y%400 != 0):
+            print("false")
+    elif (y%4 == 0):
+        print("true")
+    else:
+        print("false")
+
+year(y)
