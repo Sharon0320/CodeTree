158fe94b540e385fcba676f18d1c20d1db6c35aa
Lee Jae Yong <sharon0320@gachon.ac.kr>
2025-07-15 09:50:36 +0900

    Title: 두 정수에 대한 연산값, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 250715/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 250715/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -3,12 +3,13 @@ a, b = map(int, input().split())
 # Please write your code here.
 
 def program(a,b):
-    bigger = max(a,b)
-    smaller = min(a,b)
+    if a>b:
+        a +=25
+        b *=2
+    else:
+        b +=25
+        a*=2
 
-    bigger += 25
-    smaller *= 2
-
-    print(smaller,bigger)
+    print(a,b)
 
 program(a,b)
